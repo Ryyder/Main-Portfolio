9b0526ba0852e66f0ccc494af36865a70f2aff25
Ryyder <brucecho82@hotmail.com>
2019-04-26 21:46:19 -0700

    working on scroll animations

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,1 @@
-$(document).ready(function(){
-  $('.aniview').AniView();
-});
-
- var options = {
-  animateThreshold: 100,
-  scrollPollInterval: 50
-}
-$('.aniview').AniView(options); 
+AOS.init();
